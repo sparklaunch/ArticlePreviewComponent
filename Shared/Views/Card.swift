086f3488678ccf2cae7de0ce49b6f9cb507ea80d
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-30 20:03:45 +0900

    Add the background.

---
+++ Shared/Views/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  ArticlePreviewComponent (iOS)
+//
+//  Created by Jinwook Kim on 2022/03/30.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color("BackgroundColor")
+            Card()
+        }
+        .ignoresSafeArea()
     }
 }
 
