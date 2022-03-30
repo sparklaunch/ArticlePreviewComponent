c44f25b7454d7a446720cac2961d86c497838a64
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-30 20:12:26 +0900

    Add the texts.

--- Shared/Views/Card.swift
+++ Shared/Views/Card.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct Card: View {
     var body: some View {
-        VStack {
+        VStack(spacing: .zero) {
             Image("Drawers")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
+                .aspectRatio(contentMode: .fill)
+            ZStack {
+                Color.white
+                VStack(spacing: 15) {
+                    Text("Shift the overall look and feel by adding these wonderful touches to furniture in your home")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("TitleColor"))
+                        .fixedSize(horizontal: false, vertical: true)
+                    Text("Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.")
+                        .font(.body)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color("TitleColor"))
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+                .padding(30)
+            }
         }
+        .scaledToFit()
         .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
         .padding()
         .shadow(radius: 20)
