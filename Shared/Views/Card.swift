70187895bdce18c80c30e0d7d4d6ae2d82ff3a02
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-30 20:06:09 +0900

    Add the drawers image.

--- Shared/Views/Card.swift
+++ Shared/Views/Card.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct Card: View {
     var body: some View {
         VStack {
-            
+            Image("Drawers")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
         }
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .padding()
+        .shadow(radius: 20)
     }
 }
