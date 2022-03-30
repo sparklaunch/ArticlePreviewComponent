7329e8a44ed46dacdc764648d0d3a77023df9b50
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-30 19:52:25 +0900

    Initial Commit

---
+++ Shared/ArticlePreviewComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlePreviewComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/30.
+//
+
+import SwiftUI
+
+@main
+struct ArticlePreviewComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
